0df502961f12f95b595242b6799fedee043a0473
cs <bartus.csongor@gmail.com>
2015-12-22 19:36:26 +0200

    Tab done for landscape too

--- site/components/project/tab/tab-item/tab-item.js
+++ site/components/project/tab/tab-item/tab-item.js
@@ -1,6 +1,7 @@
-var toggleTabItem = function(tabItemTriggersID) {
+// It makes tabs behave like an accordion
+// - since the trigger is visible only in portrait mode it doesn't affects landscape mode
+var tabAccordion = function(tabItemTriggersID) {
   var tabItemTriggers = document.querySelectorAll(tabItemTriggersID);
-  console.log(tabItemTriggers.length);
 
   for (var i=0; i < tabItemTriggers.length; i++) {
     tabItemTriggers[i].addEventListener('click', clickOnTabTrigger, false);
@@ -11,4 +12,42 @@ var toggleTabItem = function(tabItemTriggersID) {
   }
 }
 
-toggleTabItem('.tab .tab-item__title .trigger');
+// portrait
+tabAccordion('.tab .tab-item__title .trigger');
+
+
+
+// Marks a tab active in landscape mode
+// - The description + content of the active tab  will be populated into the `tab__content` area below the tabs
+var toggleTabItem = function(tabItemsID, tabItemTitlesID, tabContentID) {
+  var mediaQuery = "only screen and (orientation: landscape)";
+  var tabItemTitles = document.querySelectorAll(tabItemTitlesID);
+  var tabItems = document.querySelectorAll(tabItemsID);
+  var tabContent = document.querySelector(tabContentID);
+
+  if (matchMedia(mediaQuery).matches) {
+    for (var i=0; i < tabItemTitles.length; i++) {
+      tabItemTitles[i].addEventListener('click', clickOnTabTitle, false);
+    }
+  }
+
+  function clickOnTabTitle() {
+    for (var i=0; i < tabItems.length; i++) {
+      tabItems[i].classList.remove('tab-item--visible');
+    }
+
+    this.parentNode.classList.add('tab-item--visible');
+    populateTabContent(this.parentNode, tabContent)
+  }
+
+  function populateTabContent(tab, tabContent) {
+    tabContent.innerHTML = '';
+    var description = tab.querySelector('.tab-item__description');
+    var content = tab.querySelector('.tab-item__content');
+
+    tabContent.innerHTML = description.outerHTML + content.outerHTML;
+  }
+}
+
+// landscape
+toggleTabItem('.tab .tab-item', '.tab .tab-item__title', '.tab .tab__content .populated-by-javascript');
