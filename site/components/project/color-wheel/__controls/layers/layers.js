078e2077646bbf0ddf74efc72dfb14319854c5f5
cs <bartus.csongor@gmail.com>
2015-12-29 15:21:24 +0200

    Wheel select layer control works

--- site/components/project/color-wheel/__controls/layers/layers.js
+++ site/components/project/color-wheel/__controls/layers/layers.js
@@ -4,10 +4,36 @@ var createLayerNavigation = function(layersID) {
   for (var i=0; i < wheels.length; i++) {
     var layer = document.createElement('span');
     layer.setAttribute('class', 'layer');
-    layer.setAttribute('data-index', wheels.length - i + 1);
+    layer.setAttribute('data-index', wheels.length - i - 1);
     layer.innerHTML = (i + 1);
     layers.appendChild(layer);
   }
 }
 
 createLayerNavigation('.color-wheel .layers');
+
+
+
+var selectColorWheel = function(layersID, wheelsID) {
+  var layers = document.querySelectorAll(layersID);
+  var wheels = document.querySelectorAll(wheelsID);
+
+  for (var i = 0; i < layers.length; i++) {
+    layers[i].addEventListener('click', click, false);
+  }
+
+  function click() {
+    index = this.dataset.index;
+
+    for (var i = 0; i < wheels.length; i++) {
+      wheels[i].classList.remove('wheel--selected');
+      layers[i].classList.remove('layer--selected');
+    }
+
+    this.classList.add('layer--selected');
+    wheels[index].classList.add('wheel--selected');
+  }
+}
+
+
+selectColorWheel('.color-wheel .layer', '.color-wheel .wheel');
