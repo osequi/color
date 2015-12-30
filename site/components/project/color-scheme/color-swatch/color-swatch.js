cabf4f96e9011ee42eaff943276df148222ef929
cs <bartus.csongor@gmail.com>
2015-12-30 13:58:37 +0200

    Input and button removed from the color swatches

--- site/components/project/color-scheme/color-swatch/color-swatch.js
+++ site/components/project/color-scheme/color-swatch/color-swatch.js
@@ -29,31 +29,16 @@ var colorSwatchChange = function(colorsID) {
   var colors = document.querySelectorAll(colorsID);
   colors[0].classList.add('color-swatch--active');
 
-  var names = document.querySelectorAll(colorsID + ' .color-swatch__name');
-  var buttons = document.querySelectorAll(colorsID + ' .color-swatch__button');
-  var inputs = document.querySelectorAll(colorsID + ' .color-swatch__value');
-
   for (var i=0; i < colors.length; i++) {
-    names[i].addEventListener('click', clickOnColorName, false);
-    buttons[i].addEventListener('click', clickOnButton, false);
-    inputs[i].addEventListener('click', clickOnColorName, false);
+    colors[i].addEventListener('click', click, false);
   }
 
-  function clickOnColorName() {
+  function click() {
     for (var i=0; i < colors.length; i++) {
       colors[i].classList.remove('color-swatch--active');
     }
 
-    this.parentNode.classList.add('color-swatch--active');
-  }
-
-  function clickOnButton() {
-    var color = this.previousElementSibling.value;
-    this.previousElementSibling.style.backgroundColor = color;
-
-    if (this.parentNode.dataset.name == 'light') {
-      document.body.style.backgroundColor = color;
-    }
+    this.classList.add('color-swatch--active');
   }
 }
 
